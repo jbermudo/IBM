645a6937fdedd72c5d8ff70d3b21509490fa8fbe
asdamandan <jmbermudo@student.apc.edu.ph>
2020-01-07 13:21:56 +0800

    ibm2

--- Ibm Cognitive build/app/app.js
+++ Ibm Cognitive build/app/app.js
@@ -7,8 +7,28 @@ angular.module('myApp', [
     'myApp.view2',
     'myApp.version'
 ]).
+
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
     $locationProvider.hashPrefix('!');
-
     $routeProvider.otherwise({ redirectTo: '/index' });
 }]);
+
+function checkSession() {
+    let logged_timestamp = window.localStorage.getItem('logged_timestamp');
+    let user_id = window.localStorage.getItem('id');
+    if(logged_timestamp) {
+        let now = Date.now();
+        let session_time = 7; //Days
+        //Compute log timestamp if more that 5 mins
+        let diff = logged_timestamp - now;
+        diff = diff + (((session_time * 24) * 60) * 1000); //7 days
+        // diff = diff + (session_time * 1000); //5 secs
+        const isTimeout = diff < 0;
+        // console.log(diff);
+        if (isTimeout && user_id) {
+            window.localStorage.clear();
+            window.location.href = "/index";
+        }
+    }
+}
+checkSession();
--- Ibm Cognitive build/app/view1/view1.js
+++ Ibm Cognitive build/app/view1/view1.js
@@ -7,14 +7,15 @@ app.config(['$routeProvider', function($routeProvider) {
         .when('/view1', {
             templateUrl: 'view1/view1.html',
             controller: 'loginCtrl',
-            controller: 'Logout'
+            controller: 'Logout',
+            controller: 'Profile'
         })
         .when('/dashboard', {
             templateUrl: 'dashboard.html'
         })
         .otherwise({
             redirectTo: '/'
-        })
+        });
 }])
 
 //login 
@@ -42,7 +43,14 @@ app.controller('loginCtrl', function($http, $scope, $location, $timeout, $window
                         isLogged = true;
                         $scope.error = "Login Success"
                         $timeout(function() {
-                            window.localStorage.getItem("saved", JSON.stringify(account[i]));
+                            //Store browser database
+                            window.localStorage.setItem('id', account[i].id);
+                            window.localStorage.setItem('username', account[i].username);
+                            window.localStorage.setItem('name', account[i].name);
+                            window.localStorage.setItem('skill', account[i].skill);
+                            window.localStorage.setItem('image', account[i].image);
+                            window.localStorage.setItem('type', account[i].type);
+                            window.localStorage.setItem('logged_timestamp', Date.now());
                             window.location.href = '/dashboard';
                         }, 3000);
                         break;
@@ -69,22 +77,43 @@ app.controller('loginCtrl', function($http, $scope, $location, $timeout, $window
 //logout
 app.controller('Logout', function($scope, $location) {
     $scope.logout = function() {
+        window.localStorage.clear();
         window.location.href = "/index";
+    }
+});
+//profile
+app.controller('Profile', function($scope, $location) {
+    $scope.logout = function() {
+        window.location.href = "/profile";
     }
 });
 //getting credentials
 
 app.controller('showCredentials', function($http, $scope) {
+    //Check user login exist
+    let user_id = window.localStorage.getItem('id');
+    if(!user_id) {
+        window.location.href = '/index';
+    }
+    //Get account name from local storage
+    let name = window.localStorage.getItem('name');
+    document.getElementById("account-name").innerHTML = name;
+    //Get account type from local storage
+    let type = window.localStorage.getItem('type');
+    //Iterate accounts
     $http.get('accounts.json').then(function(response) {
         console.log(response.data);
         let data = response.data;
 
         function card() {
             for (let i in data) {
-                let mainDiv = document.getElementById("deck");
+                let mainDiv = document.getElementById("controller");
+                let divCol = document.createElement("div");
+                divCol.setAttribute("class", "col-lg-3 col-sm-4");
+                divCol.setAttribute("style", "padding: 10px;");
                 let divCard = document.createElement("div");
                 divCard.setAttribute("class", "card");
-                divCard.setAttribute("style", "width: 18rem;");
+                // divCard.setAttribute("style", "width: 18rem;");
                 let imgEl = document.createElement("img");
                 imgEl.setAttribute("class", "card-img-top");
                 imgEl.setAttribute("src", data[i].image);
@@ -97,9 +126,24 @@ app.controller('showCredentials', function($http, $scope) {
                 let pText2 = document.createTextNode(data[i].skill);
                 pEl2.setAttribute("class", "card-text");
                 pEl2.append(pText2);
+                //Download Button
+                let downloadBtn = document.createElement("a");
+                let downloadBtnTxt = document.createTextNode("Download Resume");
+                downloadBtn.setAttribute("class", "btn-primary");
+                downloadBtn.setAttribute("style", "padding: 10px; display: table; margin: auto auto;");
+                if(!data[i].resume) {
+                    downloadBtn.disabled = true; 
+                } else {
+                    downloadBtn.setAttribute("href", data[i].resume);
+                }
+                downloadBtn.append(downloadBtnTxt);
+                //Append
                 divCard.append(imgEl, pEl, pEl2);
-
-                mainDiv.append(divCard);
+                if(type == "employer") {
+                    divCard.append(downloadBtn);
+                }
+                divCol.append(divCard)
+                mainDiv.append(divCol);
             }
 
         }
