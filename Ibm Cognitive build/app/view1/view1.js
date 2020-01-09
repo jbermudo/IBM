c5072b0dfae6df11c60edc63ea7c8e5919867b65
asdamandan <jmbermudo@student.apc.edu.ph>
2020-01-09 13:59:18 +0800

    ibm 2.0

--- Ibm Cognitive build/app/view1/view1.js
+++ Ibm Cognitive build/app/view1/view1.js
@@ -2,7 +2,7 @@
 
 let app = angular.module('myApp.view1', ['ngRoute'])
 
-app.config(['$routeProvider', function($routeProvider) {
+app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
         .when('/view1', {
             templateUrl: 'view1/view1.html',
@@ -10,7 +10,9 @@ app.config(['$routeProvider', function($routeProvider) {
             controller: 'Logout',
             controller: 'Profile',
             controller: 'userProfile',
-            controller: 'Home'
+            controller: 'Home',
+            controller: 'active-navlink',
+            controller: 'employer'
         })
         .when('/dashboard', {
             templateUrl: 'dashboard.html'
@@ -21,9 +23,9 @@ app.config(['$routeProvider', function($routeProvider) {
 }])
 
 //login 
-app.controller('loginCtrl', function($http, $scope, $location, $timeout, $window, $rootScope, $sce) {
-    $scope.submit = function() {
-        $http.get('accounts.json').then(function(response) {
+app.controller('loginCtrl', function ($http, $scope, $location, $timeout, $window, $rootScope, $sce) {
+    $scope.submit = function () {
+        $http.get('accounts.json').then(function (response) {
             $scope.accounts = response.data;
             let account = response.data;
             let uname = $scope.username;
@@ -44,7 +46,7 @@ app.controller('loginCtrl', function($http, $scope, $location, $timeout, $window
 
                         isLogged = true;
                         $scope.error = "Login Success! Welcome " + account[i].name;
-                        $timeout(function() {
+                        $timeout(function () {
                             //Store browser database
                             window.localStorage.setItem('id', account[i].id);
                             window.localStorage.setItem('username', account[i].username);
@@ -77,27 +79,27 @@ app.controller('loginCtrl', function($http, $scope, $location, $timeout, $window
 });
 
 //logout
-app.controller('Logout', function($scope, $location) {
-    $scope.logout = function() {
+app.controller('Logout', function ($scope, $location) {
+    $scope.logout = function () {
         window.localStorage.clear();
         window.location.href = "/index";
     }
 });
 //profile
-app.controller('Profile', function($scope, $location) {
-    $scope.profile = function() {
+app.controller('Profile', function ($scope, $location) {
+    $scope.profile = function () {
         window.location.href = "/profile";
     }
 });
 //profile
-app.controller('Home', function($scope, $location) {
-    $scope.home = function() {
+app.controller('Home', function ($scope, $location) {
+    $scope.home = function () {
         window.location.href = "/dashboard";
     }
 });
 //getting credentials
 
-app.controller('showCredentials', function($http, $scope) {
+app.controller('showCredentials', function ($http, $scope) {
     //Check user login exist
     let user_id = window.localStorage.getItem('id');
     if (!user_id) {
@@ -110,53 +112,53 @@ app.controller('showCredentials', function($http, $scope) {
     //Get account type from local storage
     let type = window.localStorage.getItem('type');
     //Iterate accounts
-    $http.get('accounts.json').then(function(response) {
+    $http.get('accounts.json').then(function (response) {
         let data = response.data;
 
         function card() {
             for (let i in data) {
-                if(type == 'employee'){
+                if (type == 'employee') {
                     console.log("employee");
-                }else{
-                let mainDiv = document.getElementById("controller");
-                let divCol = document.createElement("div");
-                divCol.setAttribute("class", "col-lg-3 col-sm-4");
-                divCol.setAttribute("style", "padding: 10px;");
-                let divCard = document.createElement("div");
-                divCard.setAttribute("class", "card");
-                // divCard.setAttribute("style", "width: 18rem;");
-                let imgEl = document.createElement("img");
-                imgEl.setAttribute("class", "card-img-top");
-                imgEl.setAttribute("src", data[i].image);
-                imgEl.setAttribute("alt", "Card image cap");
-                let pEl = document.createElement("p");
-                let pText = document.createTextNode(data[i].name);
-                pEl.setAttribute("class", "card-text");
-                pEl.append(pText);
-                let pEl2 = document.createElement("p");
-                let pText2 = document.createTextNode(data[i].skill);
-                pEl2.setAttribute("class", "card-text");
-                pEl2.append(pText2);
-                //Download Button
-                let downloadBtn = document.createElement("a");
-                let downloadBtnTxt = document.createTextNode("Download Resume");
-                downloadBtn.setAttribute("class", "btn-primary");
-                downloadBtn.setAttribute("style", "padding: 10px; display: table; margin: auto auto;");
-                if (!data[i].resume) {
-                    downloadBtn.disabled = true;
                 } else {
-                    downloadBtn.setAttribute("href", data[i].resume);
+                    let mainDiv = document.getElementById("controller");
+                    let divCol = document.createElement("div");
+                    divCol.setAttribute("class", "col-lg-3 col-sm-4");
+                    divCol.setAttribute("style", "padding: 10px;");
+                    let divCard = document.createElement("div");
+                    divCard.setAttribute("class", "card");
+                    // divCard.setAttribute("style", "width: 18rem;");
+                    let imgEl = document.createElement("img");
+                    imgEl.setAttribute("class", "card-img-top");
+                    imgEl.setAttribute("src", data[i].image);
+                    imgEl.setAttribute("alt", "Card image cap");
+                    let pEl = document.createElement("p");
+                    let pText = document.createTextNode(data[i].name);
+                    pEl.setAttribute("class", "card-text");
+                    pEl.append(pText);
+                    let pEl2 = document.createElement("p");
+                    let pText2 = document.createTextNode(data[i].skill);
+                    pEl2.setAttribute("class", "card-text");
+                    pEl2.append(pText2);
+                    //Download Button
+                    let downloadBtn = document.createElement("a");
+                    let downloadBtnTxt = document.createTextNode("Download Resume");
+                    downloadBtn.setAttribute("class", "btn-primary");
+                    downloadBtn.setAttribute("style", "padding: 10px; display: table; margin: auto auto;");
+                    if (!data[i].resume) {
+                        downloadBtn.disabled = true;
+                    } else {
+                        downloadBtn.setAttribute("href", data[i].resume);
+                    }
+                    downloadBtn.append(downloadBtnTxt);
+                    //Append
+                    divCard.append(imgEl, pEl, pEl2);
+                    if (type == "employer") {
+                        divCard.append(downloadBtn);
+                    }
+                    divCol.append(divCard)
+                    mainDiv.append(divCol);
                 }
-                downloadBtn.append(downloadBtnTxt);
-                //Append
-                divCard.append(imgEl, pEl, pEl2);
-                if (type == "employer") {
-                    divCard.append(downloadBtn);
-                }
-                divCol.append(divCard)
-                mainDiv.append(divCol);
             }
-        }
 
         }
         card();
@@ -164,7 +166,7 @@ app.controller('showCredentials', function($http, $scope) {
     })
 })
 
-app.controller("userProfile", function($http, $scope) {
+app.controller("userProfile", function ($http, $scope) {
     //get usernam,image,skill in window localStorage
     let userName = window.localStorage.getItem('name');
     let img = window.localStorage.getItem('image');
@@ -174,55 +176,90 @@ app.controller("userProfile", function($http, $scope) {
     let card = document.getElementById("card")
     //create Element image
     let image = document.createElement("img");
-    image.setAttribute("class","card-img-top");
-    image.setAttribute("src",img);
-    image.setAttribute("alt","Card image cap");
+    image.setAttribute("class", "card-img-top");
+    image.setAttribute("src", img);
+    image.setAttribute("alt", "Card image cap");
     //create element div
     let cardBody = document.createElement("div");
-    cardBody.setAttribute("class","card-body");
+    cardBody.setAttribute("class", "card-body");
     //create element h5
     let h5 = document.createElement("card-title");
-    h5.setAttribute("class","card-title");
+    h5.setAttribute("class", "card-title");
     h5.innerHTML = userName;
     //create element p
     let p = document.createElement("p");
-    p.setAttribute("class","card-text");
+    p.setAttribute("class", "card-text");
     p.innerHTML = skill;
     //append
     card.append(image, cardBody, h5, p);
 
-}) 
+})
 
 
-app.controller('job', function($http, $scope) {
-    $http.get('jobs.json').then(function(response) {
-    let data = response.data;
-    for(let i in data){
-    let container = document.getElementById("job");
-    let div = document.createElement("div");
-    div.setAttribute("class","jumbotron");
+app.controller('job', function ($http, $scope) {
+    $http.get('jobs.json').then(function (response) {
+        let data = response.data;
+        for (let i in data) {
+            let container = document.getElementById("job");
+            let div = document.createElement("div");
+            div.setAttribute("class", "jumbotron");
 
-    let span = document.createElement("span");
-    span.setAttribute("class","jobtitle")
-    span.innerHTML = data[i].jobtitle;
+            let span = document.createElement("span");
+            span.setAttribute("class", "jobtitle")
+            span.innerHTML = data[i].jobtitle;
 
-    let p = document.createElement("p");
-    p.setAttribute("class","company")
-    p.innerHTML = "<span class='companyDesc'>Company: </span> " + data[i].company;
+            let p = document.createElement("p");
+            p.setAttribute("class", "company")
+            p.innerHTML = "<span class='companyDesc'>Company: </span> " + data[i].company;
 
-    let pLoc = document.createElement("p");
-    pLoc.setAttribute("class","location")
-    pLoc.innerHTML = "<span class='spanLoc'><i class='fas fa-map-marker-alt'></i></span> "  + data[i].location;
+            let pLoc = document.createElement("p");
+            pLoc.setAttribute("class", "location")
+            pLoc.innerHTML = "<span class='spanLoc'><i class='fas fa-map-marker-alt'></i></span> " + data[i].location;
 
 
-    let pDesc = document.createElement("p");
-    pDesc.setAttribute("class","description")
-    pDesc.innerHTML = "<span class='spanDesc'>Job Description:</span> " + data[i].jobDescription;
+            let pDesc = document.createElement("p");
+            pDesc.setAttribute("class", "description")
+            pDesc.innerHTML = "<span class='spanDesc'>Job Description:</span> " + data[i].jobDescription;
 
-    div.append(span,pLoc,p,pDesc)
+            div.append(span, pLoc, p, pDesc)
 
-    container.append(div);
-    }
+            container.append(div);
+        }
     });
 });
 
+app.controller('active-navlink', function ($scope) {
+    $scope.navbarLink = function (elem) {
+        let navlink = document.getElementById("navLink");
+        let navlink2 = document.getElementById("navLink2");
+        document.getElementById("row-flex-card").style.marginTop = "-40px";
+        if (elem == "navLink") {
+            navlink.classList.add("menu-link-active");
+            navlink2.classList.remove("menu-link-active");
+
+        } else if (elem == "navLink2") {
+            navlink2.classList.add("menu-link-active");
+            navlink.classList.remove("menu-link-active");
+        }
+    }
+})
+
+app.controller("employer", function ($http) {
+
+    $http.get('employer.json').then(function (response) {
+        let data = response.data;
+        for (let i in data) {
+            let rowflexCard = document.getElementById("row-flex-card");
+            let divCol = document.createElement("div");
+            divCol.setAttribute("class", "col-lg-4 col-sm-4");
+            let a = document.createElement("a");
+            a.setAttribute("href", "#");
+            let im = document.createElement("img");
+            im.setAttribute("src", data[i].image);
+            // im.setAttribute("style", "width: 60%; position: absolute;top: 18px;")
+            divCol.append(a, im);
+            rowflexCard.append(divCol);
+
+        }
+    })
+})
